d33a241cbde1a911d102772b818450c8e763565a
LisettedB <lisettevanwijngaarden@hotmail.com>
2021-09-14 15:40:38 +0200

    worpHistory werkt

--- Speler.java
+++ Speler.java
@@ -4,6 +4,6 @@ import java.util.ArrayList;
 
 public class Speler {
 	String playerName;
-	static ArrayList<String> worpHistory = new ArrayList<String>();
+	ArrayList<String> worpHistory = new ArrayList<String>();
 	
 }
